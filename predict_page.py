96c0730b5fdbe2404d1ce86309f75d755a7f1ec8
csrafsan <rafsanahmedbd21@gmail.com>
2023-01-11 16:40:54 +0600

    prediction page successfully created!

--- predict_page.py
+++ predict_page.py
@@ -22,32 +22,39 @@ def show_predict_page():
 
     
     age = st.number_input("age", 59)
-    sex = st.number_input("sex", 1)
-    cp = st.number_input("cp", 1)
+    sex = st.slider("Male/Female", 0,1,)
+    ca = st.number_input("ca", 0)
     trestbps = st.number_input("trestbps", 140)
     chol = st.number_input("chol", 221)
-    fbs = st.number_input("fbs",  0)
-    restecge = st.number_input("restecg",  1)
-    thalach = st.number_input("thalach", 164)
-    exang = st.number_input("exang",1)
-    oldpeak =  st.number_input(label="systolic blood pressure",step=1.,format="%.1f")
-    slope = st.number_input("slope", 2)
-    ca = st.number_input("ca", 0)
-    thal = st.number_input("thal",2)
+    # fbs = st.number_input("fbs",  0)
+    # restecge = st.number_input("restecg",  1)
+    # thalach = st.number_input("thalach", 164)
+    # exang = st.number_input("exang",1)
+    # oldpeak =  st.number_input(label="systolic blood pressure",step=1.,format="%.1f")
+    # slope = st.number_input("slope", 2)
+    # ca = st.number_input("ca", 0)
+    # thal = st.number_input("thal",2)
    
     
 
     ok = st.button("Check Now")
     
     if ok:
-        X_test = np.array([[age, sex, cp,trestbps,chol,fbs,restecge,thalach,exang,oldpeak,slope,ca,thal ]])
+        X_test = np.array([[age, sex,ca,trestbps,chol]])
 
-        X_test = X_test.astype(int)
+        # X_test = X_test.astype(int)
         
-        st.subheader(f"Heart disease prediction is {X_test}")
+        # st.subheader(f"Heart disease prediction is {X_test}")
 
         
 
         y_pred_logistic = model_logistic_loaded.predict(X_test)
-        st.subheader(f"Heart disease prediction is {y_pred_logistic[0]:.1f}")
-        st.subheader(f"Heart disease prediction is {y_pred_logistic}")
+
+        if(y_pred_logistic[0]==1):
+            st.subheader("Heart disease prediction result is Positive!")
+        else:
+            st.subheader("Heart disease prediction result is Negative!")
+
+
+        # st.subheader(f"Heart disease prediction is {y_pred_logistic[0]}")
+        # st.subheader(f"Heart disease prediction is {y_pred_logistic}")
